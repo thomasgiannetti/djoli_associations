b60d30e41315a606fe64361412e08281645ab56d
Thomas Giannetti-Fakhoury <72355918+thomasgiannetti@users.noreply.github.com>
2024-04-15 23:45:39 +0000

    Update frequentitems.py

--- frequentitems.py
+++ frequentitems.py
@@ -78,7 +78,7 @@ for index, row in top_rules.iterrows():
 
     st.subheader(f"{antecedents} & {consequents}\n")
     st.write(f"There is a {confidence:.2f}% probability of finding {consequents} in a transaction given that {antecedents} is present.\n")
-    st.write(f"This item association has occurred in approximately {support:.2f} of all transactions.\n")
+    st.write(f"This item association has occurred in approximately {support:.2f}% of all transactions.\n")
     st.markdown(f"\n")
     st.divider()
 
